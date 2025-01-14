b6791702a31aa14bc1070bc5056ab769ba389c6b
pranavshinde16 <shindepranav909@gmail.com>
2025-01-14 16:38:35 +0530

    modified nextauth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -6,7 +6,7 @@ import bcrypt from "bcryptjs";
 import prisma from "@/lib/prisma";
 
 const handler = NextAuth({
-    adapter: PrismaAdapter(prisma),
+    // adapter: PrismaAdapter(prisma),
     providers: [
         CredentialsProvider({
             name: "Credentials",
